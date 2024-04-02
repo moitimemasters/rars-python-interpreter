221312d9d52de9d17706b75f08f0a037a19c6716
Ivan Pesnya <ivanpesnia04@gmail.com>
2024-04-02 17:43:04 +0300

    test mermaid

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
-def fib(n):
-    return fib(n - 1) + fib(n - 2) if n > 1 else 1
+fib = lambda n: n if n < 3 else fib(n - 1) + fib(n - 2)
 
-
-fib(8)
+fib(8)  # more than 9 is actually too much for heap size
